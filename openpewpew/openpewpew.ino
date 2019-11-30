e92ca34c3626f1d2f453dde2d856bee7caf2b46f
Björn Kempen <buffis@users.noreply.github.com>
2019-11-30 14:15:41 +0100

    Update openpewpew.ino

--- openpewpew/openpewpew.ino
+++ openpewpew/openpewpew.ino
@@ -14,36 +14,28 @@ struct AutofireConfig {
 AutofireConfig BUTTON_1 = {
   /*frames_active=*/1,
   /*frames_inactive=*/2,
-  /*input_pin=*/12,
-  /*output_pin=*/11,
+  /*input_pin=*/8,
+  /*output_pin=*/7,
   /*autofire_state=*/0  
 };
 
 AutofireConfig BUTTON_2 = {
   /*frames_active=*/3,
   /*frames_inactive=*/3,
-  /*input_pin=*/10,
-  /*output_pin=*/9,
+  /*input_pin=*/6,
+  /*output_pin=*/5,
   /*autofire_state=*/0  
 };
 
 AutofireConfig BUTTON_3 = {
-  /*frames_active=*/2,
-  /*frames_inactive=*/2,
-  /*input_pin=*/23,
-  /*output_pin=*/24,
+  /*frames_active=*/15,
+  /*frames_inactive=*/15,
+  /*input_pin=*/4,
+  /*output_pin=*/3,
   /*autofire_state=*/0  
 };
 
-AutofireConfig BUTTON_4 = {
-  /*frames_active=*/2,
-  /*frames_inactive=*/2,
-  /*input_pin=*/25,
-  /*output_pin=*/26,
-  /*autofire_state=*/0  
-};
-
-AutofireConfig *BUTTONS[] = {&BUTTON_1, &BUTTON_2, &BUTTON_3, &BUTTON_4};
+AutofireConfig *BUTTONS[] = {&BUTTON_1, &BUTTON_2, &BUTTON_3};
 
 // Code below.
 boolean handle_button(AutofireConfig* button) {
@@ -57,7 +49,7 @@ boolean handle_button(AutofireConfig* button) {
         firing = true;
     } else {
         digitalWrite (button->output_pin, LOW) ;
-        pinMode(button->output_pin, INPUT_PULLUP);
+        pinMode(button->output_pin, INPUT);
     }
     if (button->autofire_state > (button->frames_active + button->frames_inactive)) {
       button->autofire_state = 0;
@@ -65,14 +57,14 @@ boolean handle_button(AutofireConfig* button) {
   } else {
     button->autofire_state = 0;
     digitalWrite (button->output_pin, LOW) ;
-    pinMode(button->output_pin, INPUT_PULLUP);
+    pinMode(button->output_pin, INPUT);
   }
   return firing;
 }
 
 ISR(TIMER1_COMPA_vect) {
   boolean firing = false;
-  for (int i=0; i<4; i++)
+  for (int i=0; i<3; i++)
     firing |= handle_button(BUTTONS[i]);
   if (firing)
     digitalWrite (LED_BUILTIN, HIGH);
@@ -81,10 +73,10 @@ ISR(TIMER1_COMPA_vect) {
 }
 
 void setup() {
-  for (int i=0; i<4; i++) {
+  for (int i=0; i<3; i++) {
     pinMode(BUTTONS[i]->input_pin, INPUT_PULLUP);
     digitalWrite (BUTTONS[i]->output_pin, LOW) ;
-    pinMode(BUTTONS[i]->output_pin, INPUT_PULLUP);
+    pinMode(BUTTONS[i]->output_pin, INPUT);
   }
 
   pinMode(LED_BUILTIN, OUTPUT);
@@ -100,4 +92,4 @@ void setup() {
   sei();
 }
 
-void loop() {}
+void loop() { delay(1); }
